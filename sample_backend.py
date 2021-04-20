d43c74272fcda2a857040c250dff58a4b6a4a397
gary <gary0928@gmail.com>
2021-04-20 12:29:41 -0700

    Add pymongo code

--- sample_backend.py
+++ sample_backend.py
@@ -5,39 +5,88 @@ from flask_cors import CORS
 import random
 import string
 
+import pymongo
+from bson import ObjectId
+
+class Model(dict):
+    """
+    A simple model that wraps mongodb document
+    """
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                { "_id": ObjectId(self._id) }, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result :
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.remove({"_id": ObjectId(self._id)})
+            self.clear()
+            return resp
+
+class User(Model):
+    db_client = pymongo.MongoClient('localhost', 27017)  #change if your db is in another host and port
+    collection = db_client["users"]["users_list"]  #db name is 'users' and collection name is 'users_list'
+
+    def find_all(self):
+        users = list(self.collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_name(self, name):
+        users = list(self.collection.find({"name": name}))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
 app = Flask(__name__)
 CORS(app)
 
-users = {
-    'users_list':
-    [
-        {
-            'id': 'xyz789',
-            'name' : 'Charlie',
-            'job' : 'Janitor',
-        }, 
-        {
-            'id' : 'abc123', 
-            'name': 'Mac',
-            'job': 'Bouncer',
-        },
-        {
-            'id' : 'ppp222', 
-            'name': 'Mac',
-            'job': 'Professor',
-        }, 
-        {
-            'id' : 'yat999', 
-            'name': 'Dee',
-            'job': 'Aspring actress',
-        },
-        {
-            'id' : 'zap555', 
-            'name': 'Dennis',
-            'job': 'Bartender',
-        }
-   ]
-}
+# users = {
+#     'users_list':
+#     [
+#         {
+#             'id': 'xyz789',
+#             'name' : 'Charlie',
+#             'job' : 'Janitor',
+#         }, 
+#         {
+#             'id' : 'abc123', 
+#             'name': 'Mac',
+#             'job': 'Bouncer',
+#         },
+#         {
+#             'id' : 'ppp222', 
+#             'name': 'Mac',
+#             'job': 'Professor',
+#         }, 
+#         {
+#             'id' : 'yat999', 
+#             'name': 'Dee',
+#             'job': 'Aspring actress',
+#         },
+#         {
+#             'id' : 'zap555', 
+#             'name': 'Dennis',
+#             'job': 'Bartender',
+#         }
+#    ]
+# }
 
 @app.route('/')
 def hello_world():
